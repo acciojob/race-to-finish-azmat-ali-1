9ee8debb807c8e9d46605b112ee93f35d0f31fc8
Azmat-Ali-1 <114492086+azmat-ali-1@users.noreply.github.com>
2023-05-29 18:32:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
-window.promises = [];
+window.promises = [prom1,prom2,prom3,prom4,prom5];
 
 // Do not change the code above this
 // add your promises to the array `promises`
+let prom1 = new Promise((resolve,reject)=>{
+	setTimeout(()=>{
+		resolve("data");
+	},1000);
+});
+let prom2 = new Promise((resolve,reject)=>{
+	setTimeout(()=>{
+		resolve("data");
+	},2000);
+});let prom3 = new Promise((resolve,reject)=>{
+	setTimeout(()=>{
+		resolve("data");
+	},3000);
+});let prom4 = new Promise((resolve,reject)=>{
+	setTimeout(()=>{
+		resolve("data");
+	},4000);
+});let prom5 = new Promise((resolve,reject)=>{
+	setTimeout(()=>{
+		resolve("data");
+	},5000);
+});
+
+let result = Promise.any(prom1,prom2,prom3,prom4,prom5);
+let div= document.getElementById("output");
+div.innerText=result;
